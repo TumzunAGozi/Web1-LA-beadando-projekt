cda7773c8f3106706ecf6883affdce01f4e30642
TumzunAGozi <hunthomas98@gmail.com>
2025-05-02 19:48:54 +0200

    Sablon weboldal, Front Controller mintával

---
+++ bemutatkozas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bemutatkozás</title>
+<link type="text/css" rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<?php include("header.html"); ?>
+<div id="wrapper">
+    <?php include("aside.html"); ?>
+    <div id="content">
+<section>
+<h1>Bemutatkozás oldal</h1>
+<article>
+<h2>Aenean nummy odio orci</h2>
+<p>Phasellus wisi nulla...</p>
+</article>
+<article>
+<h2>Aenean nummy odio orci</h2>
+<p>Adipiscing elit praesent...</p>
+</article>
+</section>
+<?php include("footer.html"); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Címlap</title>
+    <link type="text/css" rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<?php include("header.html"); ?>
+<div id="wrapper">
+    <?php include("aside.html"); ?>
+    <div id="content">
+<section>
+<h1>Címlap oldal</h1>
+<article>
+<h2>Aenean nummy odio orci</h2>
+<p>Phasellus wisi nulla...</p>
+</article>
+<article>
+<h2>Aenean nummy odio orci</h2>
+<p>Adipiscing elit praesent...</p>
+</article>
+</section>
+<?php include("footer.html"); ?>
+</body>
+</html>
